c68a7f6e2bdfaa31b06d995b8b5620c6dfddb5f7
rkchy23 <101350205+rkchy23@users.noreply.github.com>
2023-07-28 14:48:40 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "rkchy23@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@nitsglobal.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:index.html");
+?>
